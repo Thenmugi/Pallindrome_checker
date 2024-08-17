48698908e0d2c69793c753a1cde9aaf924e5d26a
Thenmugi <142307003+Thenmugi@users.noreply.github.com>
2024-08-17 10:28:26 +0530

    Add files via upload

---
+++ pallindrome checker/script.js
@@ -0,0 +1,16 @@
+let inp="hello world"
+// let b=a.split(" ")
+// let c=b.join("")
+// let d=c
+// let e=d.split('')
+// let f=e.reverse()
+// let g=f.join('')
+// console.log(c)
+// console.log(g)
+let a=inp.split(' ')
+let b=a.join('')
+let o = b;
+let d=o.split('')
+let c=d.reverse();
+let reversed =c.join('')
+console.log(reversed)
